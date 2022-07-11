c9d0a8165a81d8dfdb40da8f8187b34dee00fa5d
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-07-11 14:48:02 -0300

    Project Cleanup
    Removing unnecessary.

--- react/finbell/src/App.js
+++ react/finbell/src/App.js
@@ -1,17 +1,9 @@
-import './App.css';
-import Evento from './components/Evento';
-import Form from './components/Form';
-
 function App() {
-
   return (
-    <div className="App">
-      <h1>Testando eventos.</h1>
-      <Evento numero="1"></Evento>
-      <Evento numero="2"></Evento>
-      <Form></Form>
+    <div>
+      <p>Finbell</p>
     </div>
-  );
+  )
 }
 
 export default App;
